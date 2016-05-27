21c7fecd3d254d9ff13f155ab14adf15a23c5335
Raymond Sapida <ray.sapida@gmail.com>
2016-05-27 10:32:17 -0700

    Add tests for users#new and users#create

--- spec/controller/user_controller_spec.rb
+++ spec/controller/user_controller_spec.rb
@@ -8,15 +8,38 @@ describe 'user CRUD routes' do
     User.create(username: 'John', email: 'string@mail.com', password: '1234')
   end
 
-  context 'users#index' do
-    it 'renders index with the usernames' do
-      get '/users'
-
+  #  context 'users#index' do
+  #    it 'renders index with the usernames' do
+  #      get '/users'
+  #
+  #      expect(last_response).to be_ok
+  #      expect(last_response.body).to match 'Erich'
+  #      expect(last_response.body).to match 'Richard'
+  #      expect(last_response.body).to match 'Ralph'
+  #      expect(last_response.body).to match 'John'
+  #    end
+  #  end
+  context 'users#new' do
+    it 'renders the index view' do
+      get '/users/new'
       expect(last_response).to be_ok
-      expect(last_response.body).to match 'Erich'
-      expect(last_response.body).to match 'Richard'
-      expect(last_response.body).to match 'Ralph'
-      expect(last_response.body).to match 'John'
+      expect(last_response.body).to match 'Create an account'
+    end
+  end
+
+  context 'users#create' do
+    it 'creates a new user' do
+      params = { user: { username: 'Name', email: 'someone@mail.com' },
+                 password: '1234' }
+      expect { post '/users', params }.to change(User.all, :size).by(1)
+    end
+
+    it 'redirects to new if incorrect params' do
+      params = { user: { username: 'Name', email: 'someone@mail.com' }}
+
+      post '/users', params
+
+      expect(last_response.body).to match 'Create an account'
     end
   end
 end
