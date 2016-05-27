59c013c2ef6c0c9a84c05dfe6bcc459f04c0abab
Raymond Sapida <ray.sapida@gmail.com>
2016-05-26 18:56:14 -0700

    Add feed that will show users posts and users following posts

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,57 +1,64 @@
 #show all users
 get '/users' do
-	@users = User.all
-	erb :'/users/user_index'
+  @users = User.all
+  erb :'/users/user_index'
 end
 
 #new user form
 get '/users/new' do
-	erb :'/users/user_new'
+  erb :'/users/user_new'
 end
 
 #create new user
 post '/users' do
-	user = User.new(username: params[:username], email: params[:email])
-	user.password = params[:password]
-	if user.save
-		session[:id] = user.id
-		redirect '/'
-	else
-		# status 400
-		# flash[:errors] = user.errors.full_messages
-		redirect '/users/new'
-	end
+  user = User.new(username: params[:username], email: params[:email])
+  user.password = params[:password]
+  if user.save
+    session[:id] = user.id
+    redirect '/'
+  else
+    # status 400
+    # flash[:errors] = user.errors.full_messages
+    redirect '/users/new'
+  end
 end
 
 #get edit page
 get '/users/:id/edit' do
-	@user = User.find(params[:id])
-	erb :'/users/user_edit'
+  @user = User.find(params[:id])
+  erb :'/users/user_edit'
 end
 
 #show a user
 get '/users/:id' do
-	@user = User.find(params[:id])
+  @user = User.find(params[:id])
+end
+
+#shows personal feed
+get '/users/:id/feed' do
+  @user = User.find(params[:id])
+  @posts = @user.feed
+  erb :'users/feed'
 end
 
 #submit user edit
 put '/users/:id' do
-	user = User.find(params[:id])
-	user.username = params[:username]
-	user.email = params[:email]
-	if user.save
-		redirect '/'
-	else
-		# flash[:errors] = user.errors.full_messages
-		redirect "/users/#{current_user.id}/edit"
-	end
+  user = User.find(params[:id])
+  user.username = params[:username]
+  user.email = params[:email]
+  if user.save
+    redirect '/'
+  else
+    # flash[:errors] = user.errors.full_messages
+    redirect "/users/#{current_user.id}/edit"
+  end
 end
 
 #delete user
 delete '/users/:id' do
-	User.find(params[:id]).destroy
-	session[:id] = nil
-	current_user = nil
+  User.find(params[:id]).destroy
+  session[:id] = nil
+  current_user = nil
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,30 @@
 class User < ActiveRecord::Base
-	validates :username, :email, :password_hash, presence: true
-	validates :email, uniqueness: true
+  validates :username, :email, :password_hash, presence: true
+  validates :email, uniqueness: true
 
-	has_many :posts
-	has_many :active, { class_name: 'Follow', foreign_key: :follower_id }
-	has_many :passive, { class_name: 'Follow', foreign_key: :followed_id }
-	has_many :followers, { through: :passive, source: :follower }
-	has_many :following, { through: :active, source: :followed }
+  has_many :posts
+  has_many :active, { class_name: 'Follow', foreign_key: :follower_id }
+  has_many :passive, { class_name: 'Follow', foreign_key: :followed_id }
+  has_many :followers, { through: :passive, source: :follower }
+  has_many :following, { through: :active, source: :followed }
 
-	def follow(other_user)
-		self.following << other_user
-	end
+  def follow(other_user)
+    self.following << other_user
+  end
 
-	include BCrypt
+  include BCrypt
 
-	def password
-		@password ||= Password.new(password_hash)
-	end
+  def password
+    @password ||= Password.new(password_hash)
+  end
 
-	def password=(new_password)
-		@password = Password.create(new_password)
-		self.password_hash = @password
-	end
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 
+  def feed
+    following_ids = "SELECT followed_id FROM follows WHERE  follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id).order(created_at: :desc)
+  end
 end
