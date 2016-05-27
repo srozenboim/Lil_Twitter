fae0a1ee84748abc7a4f2e8e74b8f16dccbe0fda
Andrew Lee & Ben & Raymond Sapida & Shirley Rozenboim <pair+adamandrew.benjaminhyw.raysapida.srozenboim@devbootcamp.com>
2016-05-27 15:48:48 -0700

    Complete retweet function

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,12 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates :body, length: {maximum:140}
 
-  def retweet(post)
-    self.retweet = post.id
+  def retweet_post(post_id)
+    self.retweet = post_id
+  end
+
+  def retweeted_from
+    User.find(Post.find(self.retweet).user_id)
   end
 end
 
